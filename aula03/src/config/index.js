e959a0fb719d6fd2ac5800023cc669c8c9faa632
notreniev <everton.canez@gmail.com>
2020-01-31 19:06:15 -0300

    criado o ttflix

--- aula03/src/config/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-    db: {
-        url: 'mongodb://tomhanks:tomhanks123@ds151007.mlab.com:51007/ttflix',
-        name: 'ttflix'
-    }
-}
--- aula03/src/repositories/test.js
+++
@@ -1,8 +0,0 @@
-import { test } from '../mongo'
-
-export const findAll = async () => {
-    const coll = await test()
-
-    const result = await coll.find().toArray()
-    return result
-}
